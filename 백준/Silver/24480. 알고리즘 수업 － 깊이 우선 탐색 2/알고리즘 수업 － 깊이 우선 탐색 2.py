c7bd930e454fd0c9051fe61a7b797110f9f505be
hopepark0221 <168812699+hopepark0221@users.noreply.github.com>
2025-05-21 16:32:26 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 660 ms, Memory: 83112 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,38 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(idx):
+    global answer, visited, graph, order
+    visited[idx] = True
+    answer[idx] = order
+    order += 1
+    
+    for i in graph[idx]:
+        if not visited[i]:
+            dfs(i)
+
+# 0. 입력
+N, M, R = map(int, input().split())
+MAX = 1000000 + 10
+graph = [[] for _ in range(N+1)]
+visited = [False]*MAX
+answer = [0]*MAX
+order = 1
+
+# 1. 그래프 채우기
+for _ in range(M):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+    
+# 2. 내림차순 정렬
+for i in range(1, N+1):
+    graph[i] = sorted(graph[i], reverse=True)
+    
+# 3. 호출
+dfs(R)
+
+# 4. 출력
+for i in range(1, N+1):
+    print(answer[i])
