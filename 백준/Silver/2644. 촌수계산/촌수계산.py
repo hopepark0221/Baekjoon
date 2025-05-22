82ca4e4a591723371557c369ea99b88f125e622e
hopepark0221 <168812699+hopepark0221@users.noreply.github.com>
2025-05-22 15:27:35 +0900

    [Silver II] Title: 촌수계산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+n = int(input())
+start, end = map(int, input().split())
+m = int(input())
+
+def dfs(index, count):
+    global visited, graph, answer, end
+    visited[index] = True
+    if index == end:
+        answer = count
+        return
+    for i in range(1, n+1):
+        if graph[index][i] and not visited[i]:
+            dfs(i, count+1)
+MAX = 100+10
+visited = [False] * MAX
+graph = [[False]* MAX for _ in range(MAX)]
+answer = -1
+
+for _ in range(m):
+    x,y = map(int, input().split())
+    graph[x][y] = True
+    graph[y][x] = True
+
+dfs(start, 0)
+print(answer)
