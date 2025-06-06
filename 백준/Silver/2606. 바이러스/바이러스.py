25411fb4d129c7d19329be0f81c00e1e84f439a8
hopepark0221 <168812699+hopepark0221@users.noreply.github.com>
2025-06-06 15:58:47 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,27 +1,31 @@
+import sys
+input = sys.stdin.readline
+
 def dfs(idx):
-    global answer, graph, visited
+    global map_, visited, N, answer
+    
     visited[idx] = True
     answer += 1
     
-    for i in range(N+1):
-        if not visited[i] and graph[idx][i]:
+    for i in range(1, N+1):
+        if not visited[i] and map_[idx][i]:
             dfs(i)
-
+            
 # 0. 입력
 N = int(input())
 M = int(input())
-graph = [[False]*(N+1) for _ in range(N+1)]
-visited = [False] * (N+1)
+map_ = [[False]*(N+1) for _ in range(N+1)]
+visited = [False]*(N+1)
 answer = 0
 
-# 1. 그래프
-for i in range(M):
+# 1. map 채우기
+for _ in range(M):
     x, y = map(int, input().split())
-    graph[x][y] = True
-    graph[y][x] = True
+    map_[x][y] = True
+    map_[y][x] = True
     
-# 2. 호출
+# 2. dfs 호출
 dfs(1)
 
-# 3. 출력
-print(answer - 1)
+# 3. answer 출력
+print(answer-1)
